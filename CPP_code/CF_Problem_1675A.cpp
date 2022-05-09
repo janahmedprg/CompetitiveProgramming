e8ef50f9ebe4cb2b4da181b9395ee4047da1d9cd
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-05-08 21:40:00 -0400

    new code

--- CPP_code/CF_Problem_1675A.cpp
+++ CPP_code/CF_Problem_1675A.cpp
@@ -1,5 +1,3 @@
-#include<iostream>
-#include <vector>
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,6 +6,23 @@ int main(){
     int t;
     cin>>t;
     for(int te = 0; te<t;te++){
-        
+        long long a,b,c,x,y;
+        cin>>a>>b>>c>>x>>y;
+        long long dD=x-a;
+        long long dC=y-b;
+        if(dD<0){
+            dD=0;
+        }
+        if(dC<0){
+            dC=0;
+        }
+        long long dT = dD+dC;
+        if(dT-c<=0){
+            cout<<"YES"<<"\n";
+        }
+        else{
+            cout<<"NO"<<"\n";
+        }
     }
+    return 0;
 }
