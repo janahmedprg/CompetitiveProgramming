86986ac5585f0898baf8996c510db7facf73374b
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-12-25 00:04:38 -0500

    fixed 1619 D

--- CodeForces/CF_Problem_1619D.cpp
+++ CodeForces/CF_Problem_1619D.cpp
@@ -1,87 +1,64 @@
-#include<iostream>
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
-int main(){
-    int tests;
-    cin>>tests;
-
-    for(int t=0;t<tests;t++){
-        int n,m;
-        cin>>m>>n;
-        int joys[m][n];
-        for (int i=0;i<m;i++){
-            for (int j=0;j<n;j++){
-                int joy;
-                cin>>joy;
-                joys[i][j] = joy;
+ 
+ 
+vector<vector<int>> p;
+ 
+bool checker(int x,int m,int n){
+    vector<bool> got(n);
+    bool flag = false;
+    for(int i = 0; i<m;++i){
+        int count = 0;
+        for(int j = 0; j<n; ++j){
+            if(p[i][j] >= x){
+                got[j] = true;
+                count += 1;
             }
         }
-        if ((n-1)>m){
-            int topByFriends[n];
-            for (int i=0; i<n;i++){
-                int top=0;
-                for (int j=0;j<m;j++){
-                    if(joys[j][i]>top){
-                        topByFriends[i]=joys[j][i];
-                        top=joys[j][i];
-                    }
-                }
-            }
-            int minByFriends = 1e9+9;
-            for(int i=0;i<n;i++){
-                if(topByFriends[i]<minByFriends){
-                    minByFriends = topByFriends[i];
-                }
-            }
-            cout<<minByFriends<<endl;
-        }
-        else{
-            int topByFriends[n];
-            for (int i=0; i<n;i++){
-                int top=0;
-                for (int j=0;j<m;j++){
-                    if(joys[j][i]>top){
-                        top=joys[j][i];
-                    }
-                }
-                topByFriends[i]=top;
-            }
-            
-            int minByFriends = 1e9+9;
-            for(int i=0;i<n;i++){
-                if(topByFriends[i]<minByFriends){
-                    minByFriends = topByFriends[i];
-                }
-            }
-            int top2ByShops[m];
-            for(int i=0;i<m;i++){
-                int tmpMax=0;
-                top2ByShops[i]=0;
-                for (int j=1;j<n;j++){
-                    if(joys[i][j]>joys[i][tmpMax]){
-                        tmpMax = j;
-                    }
-                }
-                for(int j=0;j<n;j++){
-                    if(j==tmpMax){
-                        continue;
-                    }
-                    if(joys[j][i]>top2ByShops[i]){
-                        top2ByShops[i]=joys[i][j];
-                    }
-                }
-            }
-            int maxtop2ByShops=0;
-            for(int i=0;i<m;i++){
-                if(top2ByShops[i]>maxtop2ByShops){
-                    maxtop2ByShops=top2ByShops[i];
-                }
-            }
-            cout<<min(maxtop2ByShops,minByFriends)<<endl;
+        if (count > 1){
+            flag = true;
         }
     }
-    
+    if(!flag && n>1){
+        return false;
+    }
+    bool ret = true;
+    for(bool b:got){
+        ret = ret && b;
+    }
+    return ret;
+}
+ 
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n,m;
+        cin>>m>>n;
+        p.assign(m, vector<int>(n));
+        for(int i = 0; i<m; ++i){
+            for(int j = 0; j<n; ++j){
+                int a;
+                cin>>a;
+                p[i][j] = a;
+            }
+        }
+        int mi = 1;
+        int ma = 2;
+        while (checker(ma,m,n)){
+            ma *=2;
+        }
+        while(ma - mi > 1){
+            int mid = (ma+mi)/2;
+            if (checker(mid,m,n)){
+                mi = mid;
+            }
+            else {
+                ma = mid;
+            }
+        }
+        cout<< mi<<"\n";
+    }
     return 0;
 }
