d4eefc8b582f70084026a6b67189c723a18449a1
janahmedprg <jan.ahmed.prg@gmail.com>
2023-02-01 21:00:42 -0500

    fixes

--- CodeForces/CF_1792C.cpp
+++ CodeForces/CF_1792C.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 using ll = long long;
 
+vector<int> v;
+int n;
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -11,17 +14,33 @@ int main()
     cin >> T;
     while (T--)
     {
-        int n;
         cin>>n;
-        vector<int> v(n);
-        int c =0;
+        v.clear();
+        v.resize(n+1);
         for(int i = 0; i<n;++i){
-            cin>>v[i];
+            int a;
+            cin>>a;
+            v[a] = i;
         }
-        for(int i=0; i<n/2;++i) {
-            
+        int l = (n+1)/2;
+        int r = (n+2)/2;
+        if(v[l]>v[r]){
+            cout<<l<<"\n";
+            continue;
         }
-        cout<<c<<"\n";
+        while(true){
+            if(l==1){
+                break;
+            }
+            if(v[l]>v[l-1] && v[r]<v[r+1]){
+                l-=1;
+                r+=1;
+            }
+            else{
+                break;
+            }
+        }
+        cout<<l-1<<"\n";
     }
     return 0;
 }
