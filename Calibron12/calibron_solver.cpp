f7d45b63cecc536c416637117d8eb9593a1943cf
janahmedprg <jan.ahmed.prg@gmail.com>
2026-03-19 23:12:39 -0400

    new updated

--- Calibron12/calibron_solver.cpp
+++ Calibron12/calibron_solver.cpp
@@ -4,9 +4,9 @@ using namespace std;
 using ll = long long;
 const int WIDTH = 56;
 const int HEIGHT = 56;
-stack<pair<int, pair<int,int>>> placed_order;
-vector<bool> placed_pieces(13);
-vector<vector<int>> board(HEIGHT, vector<int>(WIDTH, 0));
+vector<pair<int, pair<int,int>>> placed_order;
+vector<bool> placed_pieces(13, false);
+vector<vector<int>> board(HEIGHT+1, vector<int>(WIDTH+1, 0));
 vector<pair<int,int>> pieces = {
     {21, 14},
     {17, 14},
@@ -21,171 +21,135 @@ vector<pair<int,int>> pieces = {
     {32, 11},
     {28, 7}
 };
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> right_empty_pos;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> top_empty_pos;
 
-bool place_piece(pair<int,int> xy, pair<int,int> piece, int id) {
-    if (id < 0) {
-        piece = {piece.second, piece.first};
-    }
-    if (piece.first + xy.first >= HEIGHT || piece.second + xy.second >= WIDTH) {
-        return false;
-    }
-    bool flag = 0;
-    for (int i = xy.first; i < piece.first  + xy.first; ++i) {
-        if (flag != 0) {
-            break;
+
+bool place_piece(pair<int,int>& xy, int id) {
+    int x0 = xy.first;
+    // Add width (if id is positive it's placed normally else it's placed rotated)
+    int x1 = xy.first + ((id>0) ? pieces[id-1].first : pieces[abs(id)-1].second);
+
+    int y0 = xy.second;
+    // Add height (if id is positive it's placed normally else it's placed rotated)
+    int y1 = xy.second + ((id>0) ? pieces[id-1].second : pieces[abs(id)-1].first);
+
+    bool pushRight = true;
+    for (const auto& [id, placed_xy] : placed_order) {
+        int x0_prime = placed_xy.first;
+        int x1_prime = placed_xy.first + ((id>0) ? pieces[id-1].first : pieces[abs(id)-1].second);
+
+        int y0_prime = placed_xy.second;
+        int y1_prime = placed_xy.second + ((id>0) ? pieces[id-1].second : pieces[abs(id)-1].first);
+
+        if (!(x1 <= x0_prime || x1_prime <= x0 || y1 <= y0_prime || y1_prime <= y0) || x1 > WIDTH || y1 > HEIGHT) {
+            return false;
         }
-        for (int j = xy.second; j < piece.second + xy.second; ++j) {
-            if (board[i][j] != 0) {
-                flag = board[i][j];
-                break;
-            }
-            else {
-                board[i][j] = id;
-            }
+
+        if (x1 == x0_prime && y0 >= y0_prime && y0 <= y1_prime) {
+            pushRight = false;
         }
-    }
-    if (flag != 0) {
-        for (int i = xy.second; i < piece.second + xy.second; ++i) {
-            if (board[xy.first][i] != flag) {
-                board[xy.first][i] = 0;
-            }
-            else {
-                return false;
-            }
+
+        if (y1_prime == y0 && x0_prime >= x0 && x0_prime <= x1) {
+            board[x0_prime][y1_prime] = 0;
         }
     }
-    placed_pieces[id] = true;
-    placed_order.push({id, xy});
+
+    if (pushRight && x1 < WIDTH) {
+        right_empty_pos.push({x1, y0});
+        board[x1][y0] = 1;
+    }
+
+    if (y1 < HEIGHT) {
+        top_empty_pos.push({y1, x0});
+        board[x0][y1] = 1;
+    }
+
+    placed_order.push_back({id, {x0, y0}});
+    placed_pieces[abs(id)] = true;
     return true;
 }
 
 void remove_last_piece() {
-    pair<int, pair<int,int>> last_move = placed_order.top();
-    int id = last_move.first;
-    pair<int, int> xy = last_move.second;
-    placed_order.pop();
-    pair<int,int> piece = pieces[abs(id) - 1];
-    if (id < 0) {
-        piece = {piece.second, piece.first};
-    }
-    for (int i = xy.first; i < piece.first  + xy.first; ++i) {
-        for (int j = xy.second; j < piece.second + xy.second; ++j) {
-            board[i][j] = 0;
-        }
-    }
-    placed_pieces[id] = false;
+    auto [id, xy] = placed_order.back();
+    placed_order.pop_back();
+    int x0 = xy.first;
+    // Add width (if id is positive it's placed normally else it's placed rotated)
+    int x1 = xy.first + ((id>0) ? pieces[id-1].first : pieces[abs(id)-1].second);
+
+    int y0 = xy.second;
+    // Add height (if id is positive it's placed normally else it's placed rotated)
+    int y1 = xy.second + ((id>0) ? pieces[id-1].second : pieces[abs(id)-1].first);
+
+    board[x1][y0] = 0;
+    board[x0][y1] = 0;
+    placed_pieces[abs(id)] = false;
     return;
 }
 
-void dfs_solve(int index, pair<int, int> xy){   
-    if (place_piece(xy, pieces[index], index + 1) == true) {
-        for(auto x:board){
-            cout<<"[";
-            for(ll y: x){
-                if (y < 10) {
-                    cout<<0;
-                }
-                cout<<y<<", ";
-            }
-            cout<<"]\n";
+pair<int, int> get_next_empty_pos() {
+    pair<int, int> new_xy;
+    while (true) {
+        if (right_empty_pos.empty()) {
+            new_xy = top_empty_pos.top();
+            top_empty_pos.pop();
+            new_xy = {new_xy.second, new_xy.first};
         }
-        cout<<"--------------------------------------------------------------------------------------------------------------------------------------\n";
+        else{
+            new_xy = right_empty_pos.top();
+            right_empty_pos.pop();
+        }
+        if (board[new_xy.first][new_xy.second] == 1) {
+            return new_xy;
+        }
+    }
+    return {-1, -1};
+}
+
+void dfs_solve(int index, pair<int, int> xy){
+    if (place_piece(xy, index) == true) {
+        pair<int, int> new_xy = get_next_empty_pos();
+        for (int i = 1; i< placed_pieces.size(); ++i) {
+            if (!placed_pieces[i]) {
+                dfs_solve(i, new_xy);
+            }
+        } 
         if (placed_order.size() == pieces.size()) {
             return;
         }
-        pair<int, int> new_xy = {-1, -1};
-        int x = xy.first;
-        int y = (xy.second + pieces[index].second);
-        while (new_xy.first == -1) {
-            if (y == WIDTH) {
-                y = 0;
-                x += 1;
-            }
-
-            if (board[x][y] == 0) {
-                new_xy = {x,y};
-            }
-
-            y += 1;
-        }
-        for (int i = 0; i < pieces.size(); ++i) {
-            if (placed_order.size() == pieces.size()) {
-                return;
-            }
-            if (i == index || placed_pieces[i+1] == true) {
-                continue;
-            }
-            dfs_solve(i, new_xy);
-        }
+        remove_last_piece();
     }
-    if (placed_order.size() == pieces.size()) {
-        return;
-    }
-    remove_last_piece();
-    if (place_piece(xy, pieces[index], -(index + 1)) == true) {
-        for(auto x:board){
-            cout<<"[";
-            for(ll y: x){
-                if (y < 10) {
-                    cout<<0;
-                }
-                cout<<y<<", ";
+
+
+    if (place_piece(xy, -(index)) == true) {
+        pair<int, int> new_xy = get_next_empty_pos();
+        for (int i = 1; i< placed_pieces.size(); ++i) {
+            if (!placed_pieces[i]) {
+                dfs_solve(i, new_xy);
             }
-            cout<<"]\n";
-        }
-        cout<<"--------------------------------------------------------------------------------------------------------------------------------------\n";
+        }  
         if (placed_order.size() == pieces.size()) {
             return;
         }
-        pair<int, int> new_xy = {-1, -1};
-        int x = xy.first;
-        int y = (xy.second + pieces[index].second);
-        while (new_xy.first == -1) {
-            if (y == WIDTH) {
-                y = 0;
-                x += 1;
-            }
-            if (board[x][y] == 0) {
-                new_xy = {x,y};
-            }
-
-            y += 1;
-        }
-        for (int i = 0; i < pieces.size(); ++i) {
-            if (placed_order.size() == pieces.size()) {
-                return;
-            }
-            if (i == index || placed_pieces[i+1] == true) {
-                continue;
-            }
-            dfs_solve(i, new_xy);
-        }
+        remove_last_piece();
     }
-    if (placed_order.size() == pieces.size()) {
-        return;
-    }
-    remove_last_piece();
 }
 
 int main()
 {
-    for(int i = 0; i < 12; ++i) {
+    for(int i = 1; i < placed_pieces.size(); ++i) {
+        board[0][0] = 1;
+        right_empty_pos.push({0,0});
         dfs_solve(i, {0, 0});
-        if (placed_order.size() == 12) {
+        if (placed_order.size() == pieces.size()) {
             break;
         }
     }
-    for(auto x:board){
-        cout<<"[";
-        for(ll y: x){
-            if (y < 10) {
-                cout<<0;
-            }
-            cout<<y<<", ";
-        }
-        cout<<"]\n";
+
+    int orderCount = 1;
+    for (const auto& [id, xy] : placed_order) {
+        cout<<"Placement "<<orderCount<<": id="<<id<<", x="<<xy.first<<", y="<<xy.second<<"\n";
     }
-    cout<<"--------------------------------------------------------------------------------------------------------------------------------------\n";
 
     return 0;
 }
